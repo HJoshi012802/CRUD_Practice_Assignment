421a21eebea2e8a4aa8188a1da68398087f09b6a
Harshit Joshi28 <harshitjoshi2002@gmail.com>
2023-11-25 14:24:13 +0530

    frontend Deploy

--- client/src/component/Card.js
+++ client/src/component/Card.js
@@ -8,7 +8,7 @@ export default function Card({vendor, index, clientId}){
     const DeletePost = async() => {
         const data={clientId: clientId};
         console.log(clientId);
-        await fetch(`http://localhost:8080/deleteVendor/${id}`,{
+        await fetch(`https://crud-practice-assignment-bto7sjzeq.vercel.app/deleteVendor/${id}`,{
           method:'DELETE',
           headers: {'Content-Type': 'application/json',},
           body: JSON.stringify(data),
